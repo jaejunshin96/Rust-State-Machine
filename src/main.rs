91b64f096582bea6ce841c7703275d70e77bd5a0
Jaejun Shin <jaeshinkorean@gmail.com>
2025-02-13 20:56:53 +1030

    add dispatch call to handle runtime transfer

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod balances;
 mod system;
 mod support;
 
+use crate::support::Dispatch;
+
 mod types {
-    use crate::support;
+	use crate::support;
 
 	pub type AccountId = String;
 	pub type Balance = u128;
@@ -15,7 +17,7 @@ mod types {
 }
 
 pub enum RuntimeCall {
-
+	BalancesTransfer { to: types::AccountId, amount: types::Balance },
 }
 
 #[derive(Debug)]
@@ -41,6 +43,46 @@ impl Runtime {
 			system: system::Pallet::new(),
 			balances: balances::Pallet::new()
 		}
+	}
+	// Execute a block of extrinsics. Increments the block number.
+	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+		self.system.inc_block_number();
+		if block.header.block_number != self.system.block_number() {
+			return Err("Block numbers don't match");
+		}
+		for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+			// do stuff with `caller` and `call`
+			self.system.inc_nonce(&caller);
+			let _res = self.dispatch(caller, call).map_err(|e| {
+				eprintln!(
+					"Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+					block.header.block_number, i, e
+				)
+			});
+		}
+		Ok(())
+	}
+}
+
+impl crate::support::Dispatch for Runtime {
+	type Caller = <Runtime as system::Config>::AccountId;
+	type Call = RuntimeCall;
+	// Dispatch a call on behalf of a caller. Increments the caller's nonce.
+	//
+	// Dispatch allows us to identify which underlying module call we want to execute.
+	// Note that we extract the `caller` from the extrinsic, and use that information
+	// to determine who we are executing the call on behalf of.
+	fn dispatch(
+		&mut self,
+		caller: Self::Caller,
+		runtime_call: Self::Call,
+	) -> support::DispatchResult {
+		match runtime_call {
+			RuntimeCall::BalancesTransfer { to, amount } => {
+				self.balances.transfer(caller, to, amount)?;
+			}
+		}
+		Ok(())
 	}
 }
 
